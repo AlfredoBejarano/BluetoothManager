cdf7a8a734c45764e954e0421dc274f44598e473
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-11-06 17:01:16 -0600

    Create AppDatabaseTest.kt
    Test class that asserts the database builds correctly

---
+++ app/src/test/java/me/alfredobejarano/bluethootmanager/data/AppDatabaseTest.kt
@@ -0,0 +1,37 @@
+package me.alfredobejarano.bluethootmanager.data
+
+import androidx.room.Room
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
+/**
+ * Test class for the [AppDatabase] class.
+ *
+ * @author Alfredo Bejarano
+ * @version 1.0
+ * @since November 06, 2018 - 16:24
+ */
+@RunWith(RobolectricTestRunner::class)
+class AppDatabaseTest {
+    private lateinit var testDB: AppDatabase
+
+    @Before
+    fun setup() {
+        // Builds the test database for this class.
+        testDB = Room
+            .inMemoryDatabaseBuilder(RuntimeEnvironment.systemContext, AppDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+    }
+
+    // Asserts that the database instance gets built correctly.
+    @Test
+    fun getDeviceDao() {
+        AppDatabase
+            .getInstance(RuntimeEnvironment.systemContext)
+            .getDeviceDao()
+    }
+}
