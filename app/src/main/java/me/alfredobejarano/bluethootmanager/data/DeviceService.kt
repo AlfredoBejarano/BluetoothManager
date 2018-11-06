19c6faf3f0216f8deafac885856b1848cb2719ea
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-11-06 10:18:52 -0600

    Create DeviceService.kt
    API definitions for Retrofit that will handle API calls for the devices

---
+++ app/src/main/java/me/alfredobejarano/bluethootmanager/data/DeviceService.kt
@@ -0,0 +1,31 @@
+package me.alfredobejarano.bluethootmanager.data
+
+import androidx.lifecycle.LiveData
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+/**
+ *
+ * API definitions to be implemented by a Retrofit client that
+ * allows access to said endpoints from a remote API.
+ *
+ * @author Alfredo Bejarano
+ * @since November 06, 2018 - 10:09
+ * @version 1.0
+ **/
+interface DeviceService {
+    /**
+     * Calls the remote API to retrieve the list of stored devices.
+     */
+    @GET("devices/")
+    fun fetchDevices(): LiveData<List<Device>>
+
+    /**
+     * Save the device to the remote API.
+     * **Note:** It returns the saved device itself, if it
+     * already exists, it returns the same device with the
+     * "created_at" property updated.
+     */
+    @POST("add/")
+    fun addDevice(): LiveData<Device>
+}
