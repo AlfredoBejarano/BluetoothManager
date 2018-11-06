6f28d138b38d6c351be8a460e47aed70e84e70d8
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-11-06 09:54:31 -0600

    Create Device.kt
    Data class that serves as a Model for Retrofit and an Entity for Room

---
+++ app/src/main/java/me/alfredobejarano/bluethootmanager/data/Device.kt
@@ -0,0 +1,33 @@
+package me.alfredobejarano.bluethootmanager.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ *
+ * Data class that represents a bluetooth entity for Room and a Model for retrofit.
+ *
+ * @author Alfredo Bejarano
+ * @since November 06, 2018 - 09:44
+ * @version 1.0
+ **/
+@Entity(tableName = "device_table")
+data class Device(
+    @Expose
+    @SerializedName("name")
+    val name: String,
+    @Expose
+    @SerializedName("strength")
+    val strength: Int,
+    @Expose
+    @ColumnInfo(name = "pk")
+    @SerializedName("address")
+    @PrimaryKey(autoGenerate = false)
+    val address: String,
+    @Expose
+    @SerializedName("created_at")
+    val createdAt: String
+)
