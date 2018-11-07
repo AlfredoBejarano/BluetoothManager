d9546de2ced01a6db011f498160df4ddc668db32
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-11-06 23:04:37 -0600

    Create DeviceDiffCallback.kt
    DiffUtils callback that will report updates to a list of devices to a DeviceAdapter class

---
+++ app/src/main/java/me/alfredobejarano/bluethootmanager/callback/DeviceDiffCallback.kt
@@ -0,0 +1,32 @@
+package me.alfredobejarano.bluethootmanager.callback
+
+import androidx.recyclerview.widget.DiffUtil
+import me.alfredobejarano.bluethootmanager.data.Device
+
+/**
+ *
+ * [Write your documentation here]
+ *
+ * @author Alfredo Bejarano
+ * @since November 06, 2018 - 22:45
+ * @version 1.0
+ **/
+class DeviceDiffCallback(
+    private val oldItems: List<Device>?,
+    private val newItems: List<Device>?
+) : DiffUtil.Callback() {
+    override fun getOldListSize() = oldItems?.size ?: 0
+    override fun getNewListSize() = newItems?.size ?: 0
+
+    /**
+     * Checks if two device items are the same.
+     */
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        newItems?.get(newItemPosition)?.compareTo(oldItems?.get(oldItemPosition) ?: Device()) == 0
+
+    /**
+     * Checks if two device object contents are the same.
+     */
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        oldItems?.get(oldItemPosition)?.hasTheSameContentAs(newItems?.get(newItemPosition) ?: Device()) == true
+}
