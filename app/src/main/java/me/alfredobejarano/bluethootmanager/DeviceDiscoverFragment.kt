4e875592611832b678f9e3eae607fc78b1ca2659
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-11-06 22:39:11 -0600

    Update DeviceDiscoverFragment.kt
    Listens to found devices actions and for fetching devices from the ViewModel

--- app/src/main/java/me/alfredobejarano/bluethootmanager/DeviceDiscoverFragment.kt
+++ app/src/main/java/me/alfredobejarano/bluethootmanager/DeviceDiscoverFragment.kt
@@ -1,31 +1,91 @@
 package me.alfredobejarano.bluethootmanager
 
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice.ACTION_FOUND
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_device_discover.*
+import me.alfredobejarano.bluethootmanager.adapter.DeviceAdapter
+import me.alfredobejarano.bluethootmanager.utilities.Injector
+import me.alfredobejarano.bluethootmanager.viewmodel.DeviceDiscoverViewModel
+import javax.inject.Inject
 
 /**
  * A simple [Fragment] subclass.
  *
  */
 class DeviceDiscoverFragment : Fragment() {
+    @Inject
+    lateinit var factory: DeviceDiscoverViewModel.Factory
+    private val btAdapter = BluetoothAdapter.getDefaultAdapter()
+    private val deviceFoundFilter = IntentFilter(ACTION_FOUND)
+    private lateinit var viewModel: DeviceDiscoverViewModel
+
+    /**
+     * Broadcast receiver that will listen for found bluetooth devices.
+     */
+    private val mDeviceFoundReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            // Get the intent action.
+            val action = intent?.action
+            // Is the action for a found device?
+            if (ACTION_FOUND == action) {
+                // If it is, fetch the device from the intent.
+                viewModel.reportFoundDevice(intent)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inject this fragment dependencies.
+        Injector.inject(this)
+        // Fetch the ViewModel from the activity.
+        viewModel = ViewModelProviders.of(requireActivity(), factory)[DeviceDiscoverViewModel::class.java]
+        // Observe changes in the found devices.
+        viewModel.devices.observe(this, Observer { devices ->
+            // Create a new adapter if the list adapter is null or update the existing one.
+            device_list?.adapter?.let {
+                // Update the list.
+                (device_list?.adapter as DeviceAdapter).updateList(devices)
+            } ?: run {
+                // Or create a new adapter if the list adapter is null.
+                device_list?.adapter = DeviceAdapter(devices, true)
+            }
+        })
+        // fetch the bonded devices
+        viewModel.readBondedDevices()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_device_discover, container, false)
     }
 
+    /**
+     * Starts discovering devices when the fragment starts.
+     */
+    override fun onStart() {
+        super.onStart()
+        requireContext().registerReceiver(mDeviceFoundReceiver, deviceFoundFilter)
+        btAdapter.startDiscovery()
+    }
 
+    /**
+     * Stops discovering devices if the fragment gets paused.
+     */
+    override fun onStop() {
+        super.onStop()
+        requireContext().unregisterReceiver(mDeviceFoundReceiver)
+        btAdapter.cancelDiscovery()
+    }
 }
